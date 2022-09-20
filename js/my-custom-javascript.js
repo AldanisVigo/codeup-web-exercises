77ff9b20f5b0c635898290fd4e0d591ee53d8422
Aldanis Vigo <aldanisvigo@gmail.com>
2022-09-19 19:41:33 -0500

    Use jQuery to select an element by the id. Alert the contents of the element.

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -4,3 +4,8 @@ $(function() {
     //Passing an anonymous function to the $ jquery object, will run that function after the DOM loads
     alert("The DOM has finished loading")
 });
+
+/*
+    Use jQuery to select an element by the id. Alert the contents of the element.
+ */
+alert($("#mydiv").html())
