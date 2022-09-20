cd0f3960e4b0f451f24db17b233f63c9f11bee58
Aldanis Vigo <aldanisvigo@gmail.com>
2022-09-19 19:49:58 -0500

    Using jQuery, create a border around all elements with the class codeup that is 1 pixel wide and red.

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -2,22 +2,29 @@
 
 $(function() {
     //Passing an anonymous function to the $ jquery object, will run that function after the DOM loads
-    alert("The DOM has finished loading")
+    // alert("The DOM has finished loading")
+    /*
+    Use jQuery to select an element by the id. Alert the contents of the element.
+     */
+    //alert($("#mydiv").html())
+
+        /*
+            Update the jQuery code to select and alert a different id.
+         */
+    //alert($('#myh1').html())
+
+        /*
+            Use the same id on 2 elements. How does this change the jQuery selection?
+            //Answer : I only get access to the first element
+         */
+    // Remove your custom jQuery code from previous exercises.
+    // alert($('#myli').html())
+
+    /*
+        Using jQuery, create a border around all elements with the class codeup that is 1 pixel wide and red.
+     */
+    $('.codeup').css({
+        border : '1px solid red'
+    })
 });
 
-/*
-    Use jQuery to select an element by the id. Alert the contents of the element.
- */
-//alert($("#mydiv").html())
-
-/*
-    Update the jQuery code to select and alert a different id.
- */
-//alert($('#myh1').html())
-
-/*
-    Use the same id on 2 elements. How does this change the jQuery selection?
-    //Answer : I only get access to the first element
- */
-// Remove your custom jQuery code from previous exercises.
-// alert($('#myli').html())
