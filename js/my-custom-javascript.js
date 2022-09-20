f0ca7389763378beaf0f024f4e8d88b9c276a87e
Aldanis Vigo <aldanisvigo@gmail.com>
2022-09-19 19:42:42 -0500

    Update the jQuery code to select and alert a different id.

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -8,4 +8,9 @@ $(function() {
 /*
     Use jQuery to select an element by the id. Alert the contents of the element.
  */
-alert($("#mydiv").html())
+//alert($("#mydiv").html())
+
+/*
+    Update the jQuery code to select and alert a different id.
+ */
+alert($('#myh1').html())
