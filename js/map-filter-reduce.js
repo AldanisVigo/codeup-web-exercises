c6ddd6140b459675656ad51538de7af4d5a302cb
Aldanis Vigo <aldanisvigo@gmail.com>
2022-10-31 12:54:08 -0500

    Use .filter to create an array of user objects where each user object has at least 3 languages in the languages array.

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -35,3 +35,8 @@ const users = [
         yearsOfExperience: 9
     }
 ];
+
+// Use .filter to create an array of user objects where each user object has at least 3 languages in the languages array.
+let usersWithAtLeastThreeLanguages = users.filter(user=>user.languages.length > 3)
+
+// Use .map to create an array of strings where each element is a user's email address
