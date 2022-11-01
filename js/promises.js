d7fe5e4954185d6711601ea39c137e52d8b81f17
Aldanis Vigo <aldanisvigo@gmail.com>
2022-10-31 23:18:09 -0500

    refactored weather_map.html separated functions and worked on improving logic, made marker draggable, everything is asynchronous

--- js/promises.js
+++ js/promises.js
@@ -4,29 +4,19 @@ const fetchLastCommitDate = async () => {
     try {
         const response = await fetch('https://api.github.com/users', {headers: {'Authorization': `${token}`}})
         const responsejson = await response.json()
-        console.log(responsejson)
         let reposUrl = responsejson[0].repos_url
-        console.log(reposUrl)
-        // // let eventsUrl = responsejson[0].received_events_url
-        // let reposList = await fetch(reposUrl, {headers: {'Authorization': token}})
-        // let reposListJson = await reposList.json()
-        // let username = reposListJson[0].owner.login
-        //
-        //
-        // let repoName = reposListJson[0].name
-        // let url = `https://api.github.com/repos/${username}/${repoName}/commits`
-        // // console.log(url)
-        // const repoCommits = await fetch(url,{
-        //     headers : {
-        //         'Authorization' : token
-        //     }
-        // })
-        //
-        // const repoCommitsJson = await repoCommits.json()
-        // console.log(await repoCommits.json())
-
-        // let lastEvent = eventsListJson[eventsListJson.length - 1]
-        // console.log(lastEvent.created_at.split('T')[0]);
+        let reposList = await fetch(reposUrl, {headers: {'Authorization': token}})
+        let reposListJson = await reposList.json()
+        let username = reposListJson.at(0).owner.login
+        let repoName = reposListJson.at(0).name
+        let url = `https://api.github.com/repos/${username}/${repoName}/commits`
+        const repoCommits = await fetch(url,{
+            headers : {
+                'Authorization' : token
+            }
+        })
+        let commits = await repoCommits.json()
+        console.log(commits.at(0).commit.author.date.split('T')[0])
     }catch(err){
         console.error(err)
     }
